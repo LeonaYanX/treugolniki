cfb05d0699a7484075a76f2c5f0167ee1666fc45
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-01-01 18:47:39 +0400

    Treugolniki

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿
+System.Console.WriteLine("Enter the heigth of triangle");
+int heigth = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter the width of triangle");
+int width = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i<= heigth ; i++)
+{
+//*
+//**
+//***
+//****
+//*****
+    for( int j = 1; j<=i ; j++)
+    {
+        System.Console.Write("*");
+    }
+    System.Console.WriteLine();
+}
+System.Console.WriteLine();
+for (int i = 1; i<= heigth ; i++)
+{
+// *****
+// ****
+// ***
+// **
+// *
+
+    for( int j = 0; j<=width -i ; j++)
+    {
+        System.Console.Write("*");
+    }
+    System.Console.WriteLine();
+}
+System.Console.WriteLine();
+
+for (int i = 1; i<= heigth ; i++)
+{
+//      *
+//     **
+//    ***
+//   ****
+//  *****
+
+
+    for( int j = 0; j<=width -i ; j++)
+    {
+        System.Console.Write(" ");
+
+    }
+    for(int k=1 ; k<=i ; k++)
+    {
+System.Console.Write("*");
+    }
+    System.Console.WriteLine();
+}
+System.Console.WriteLine();
+
+for (int i = 1; i<= heigth ; i++)
+{
+//  *****
+//   ****
+//    ***
+//     **
+//      *
+
+    for( int j = 1; j<=i ; j++)
+    {
+        System.Console.Write(" ");
+    }
+    for(int k = 0;k<=width-i ;k++)
+    {
+System.Console.Write("*");
+    }
+    System.Console.WriteLine();
+}
+
+
+
+
